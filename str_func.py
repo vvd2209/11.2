123e00da35f6057a811c842d828bc9febf5d2897
Valeriya Dudchenko <vvd.2209@bk.ru>
2023-08-08 21:42:37 +0300

    Дабвлена функция, которая принимает строку и возвращает ее с заглавными первыми буквами каждого слова. Добавлены докстринги к обеим функциям

--- str_func.py
+++ str_func.py
@@ -1,7 +1,22 @@
 def capital_letters(words):
+    """
+    Функция, которая принимает на вход строку и возвращает ее со всеми заглавными буквами
+    """
     string_words = words.upper()
     return string_words
 
 string_words = input()
 result = capital_letters(string_words)
+print(result)
+
+def capital_first_letters(words):
+    """
+    Функция, которая принимает на вход строку и
+    возвращает ее с заглавными первыми буквами каждого слова
+    """
+    string_words = words.title()
+    return string_words
+
+string_words = input()
+result = capital_first_letters(string_words)
 print(result)
